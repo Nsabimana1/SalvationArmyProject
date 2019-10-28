78bdd542c918f51218e61c83c3300cac37debcff
Innocent <nsabimanaii@hendrix.edu>
2019-10-27 21:45:58 -0500

    created a new route

--- SalvationArmyProject/Controllers/HomeController.cs
+++ SalvationArmyProject/Controllers/HomeController.cs
@@ -20,6 +20,10 @@ namespace SalvationArmyProject.Controllers
             return View();
         }
 
+        public IActionResult Settings() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
