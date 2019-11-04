fbb7da5d1379e4d63cb7254fa57522e886f00f42
Innocent <nsabimanaii@hendrix.edu>
2019-11-03 20:33:26 -0600

    added mss database and seed data

---
+++ SalvationArmyProject/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalvationArmyProject.Entities
+{
+    public class User
+    {
+        public Guid id { get; set; }
+        public string firstName { get; set; }
+        public string  lastName { get; set; }
+        public string  email { get; set; }
+        public DateTime birthDate { get; set; }
+        public string phoneNumber { get; set; }
+        public string userPrivilage { get; set; }
+
+    }
+}
