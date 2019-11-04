06388df1c32b53d21d122c3ac87783d945394cf4
Innocent <nsabimanaii@hendrix.edu>
2019-11-03 20:33:26 -0600

    added mss database and seed data

---
+++ SalvationArmyProject/Entities/DBcontext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalvationArmyProject.SeedData;
+
+namespace SalvationArmyProject.Entities
+{
+    public class DBcontext : DbContext
+    {
+        public DBcontext(DbContextOptions<DBcontext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            //modelBuilder.Seed();
+        //}
+    }
+}
---
+++ SalvationArmyProject/Services/UserInfoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalvationArmyProject.Entities;
+
+namespace SalvationArmyProject.Services
+{
+    public class UserInfoRepository :IUserInfoRepository
+    {
+        DBcontext _dBcontext;
+        public UserInfoRepository(DBcontext dBcontext) {
+            _dBcontext = dBcontext;
+        }
+            
+        public bool userExist(Guid userId)
+        {
+            var user = _dBcontext.Users.FirstOrDefault(u => u.id == userId);
+            return user != null;
+        }
+
+        public User getUser(Guid userId)
+        {
+           return _dBcontext.Users.FirstOrDefault(u => u.id == userId);
+        }
+
+        public IEnumerable<User> allUsers()
+        {
+            return _dBcontext.Users;
+        }
+
+        public void deleteUser(Guid userId)
+        {
+            var user = this.getUser(userId);
+            _dBcontext.Users.Remove(user);
+        }
+
+        public void addUser(User user)
+        {
+            _dBcontext.Users.Add(user);
+        }
+
+        public void updateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
