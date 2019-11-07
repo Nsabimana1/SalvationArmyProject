569fa16a688e41fe341f7c4b56427e26e00bef35
Innocent <nsabimanaii@hendrix.edu>
2019-11-07 14:45:00 -0600

    fixed errors

--- SalvationArmyProject/Program.cs
+++ SalvationArmyProject/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace SalvationArmyProject
 {
