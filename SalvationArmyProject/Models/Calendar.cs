e5b1e8484d12130d8a98972c23237f9475e00804
Will Harris <william.harris108@gmail.com>
2019-11-07 12:56:33 -0600

    More work on Calendar

--- SalvationArmyProject/Models/Calendar.cs
+++ SalvationArmyProject/Models/Calendar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -31,3 +32,17 @@ using System.Runtime.InteropServices;
 //
 // [assembly: AssemblyVersion("1.0.0.0")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace SalvationArmyProject.Models
+{
+
+    public class Events
+    {
+        public Guid id { get; set; }
+
+        public string eventName { get; set; }
+
+
+
+    }
+}
