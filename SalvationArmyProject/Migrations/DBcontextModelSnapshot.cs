31549e4b4e07758016d6a8a7c73132668f280b57
Cam DiMaggio <cam.dimaggio@yahoo.com>
2019-11-25 10:05:20 -0600

    starting index.cshtml AND deleted the images folder not in wwroot

--- SalvationArmyProject/Migrations/DBcontextModelSnapshot.cs
+++ SalvationArmyProject/Migrations/DBcontextModelSnapshot.cs
@@ -15,8 +15,7 @@ namespace SalvationArmyProject.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
                 {
