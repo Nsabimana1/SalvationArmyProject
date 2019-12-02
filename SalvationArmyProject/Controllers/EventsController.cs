0b009a54662ba4c1d9e1bcd1bfb3f56e480d7ae9
Will Harris <william.harris108@gmail.com>
2019-12-01 21:28:14 -0600

    Events things I forgot to commit until now

---
+++ SalvationArmyProject/Controllers/EventsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalvationArmyProject.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            //Create new event using some data structure
+            //Make a boolean that makes sure that every part is filled out
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Events/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int id)
+        {
+            //Remove it from datastructure/database
+            return View();
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
