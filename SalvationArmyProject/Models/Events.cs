d209d93fd0bffc97e327760e2e8527ddc1efe951
Will Harris <william.harris108@gmail.com>
2019-12-03 18:40:56 -0600

    Tried to get EventController Create action

--- SalvationArmyProject/Models/Events.cs
+++ SalvationArmyProject/Models/Events.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
